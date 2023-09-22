263b6c98cd4a5f4e5c12bb5dd2345cc89d869d3d
richmelody <chigs1million@gmail.com>
2023-09-22 08:39:32 +0100

    move

--- git-commit.sh
+++ git-commit.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-# Check if there are any changes to commit
-if [[ -n $(git status -s) ]]; then
-	  # Prompt for a commit message
-	    read -p "Enter a commit message: " commit_message
+echo "Enter a commit message:"
+read commit_message
 
-	      # Add all changes, commit with the entered message, and push to the current branch
-	        git add .
-		  git commit -m "$commit_message"
-		    git push origin $(git symbolic-ref --short HEAD)
-		      
-		      echo "Changes committed and pushed to the current branch."
-	      else
-		        echo "No changes to commit."
-fi
-
+git add .
+git commit -m "$commit_message"
+git push
