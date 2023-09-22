c83b92a2bc5738294b5ff2c41d4cc95be192f794
richmelody <chigs1million@gmail.com>
2023-09-22 08:44:21 +0100

    f

--- git-commit.sh
+++ git-commit.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-echo "Enter a commit message:"
-read commit_message
+# Check if there are any changes to commit
+if [[ $(git status --porcelain) ]]; then
+	    # Prompt for a commit message
+	        echo "Enter a commit message:"
+		    read commit_message
+	    else
+		        # No changes to commit, so skip the commit message prompt
+			    commit_message=""
+fi
 
+# Add all changes to the staging area
 git add .
+
+# Commit the changes with the specified commit message
 git commit -m "$commit_message"
+
+# Push the changes to the remote repository
 git push
