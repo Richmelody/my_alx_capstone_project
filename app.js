838e80be2e497911df380620cd6f0b50cba4915d
richmelody <chigs1million@gmail.com>
2023-10-04 14:31:10 +0100

    moving forward

--- app.js
+++ app.js
@@ -1,115 +1,84 @@
 function toggleMenu() {
+  // Your existing toggleMenu function code here
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+
+  // Adjust the navigation height when scrolling
+  const nav = document.getElementById("desktop-nav");
+  if (window.scrollY > 0) {
+    nav.style.height = "10vh"; // Adjust this value as needed
+  } else {
+    nav.style.height = "17vh"; // Original height
+  }
 }
+
+// Scroll to top button function
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+// Handle scroll to top button visibility
+window.addEventListener("scroll", function () {
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (window.scrollY > 100) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+// Check if the screen width is greater than a certain threshold (e.g., 768px)
+function isDesktop() {
+  return window.innerWidth > 768; // You can adjust the threshold as needed
+}
+
+// Attach the scroll event listener conditionally for desktop screens
+if (isDesktop()) {
+  window.addEventListener("scroll", toggleMenu);
+  window.addEventListener("scroll", handleScrollToTopButton);
+}
+
+// Function to handle the mobile menu toggle
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+  // Adjust the navigation height when scrolling
+  const nav = document.getElementById("desktop-nav");
+  if (window.scrollY > 0) {
+    nav.style.height = "10vh"; // Adjust this value as needed
+  } else {
+    nav.style.height = "17vh"; // Original height
+  }
+}
+
+// Function to handle scroll to top button visibility
+function handleScrollToTopButton() {
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (window.scrollY > 100) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
 // Function to scroll to the top of the page
 function scrollToTop() {
   window.scrollTo({
     top: 0,
-    behavior: "smooth", // Smooth scrolling animation
+    behavior: "smooth",
   });
 }
 
-// Function to show/hide the button based on scroll position
-function toggleScrollToTopButton() {
-  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-  if (window.scrollY > 600) {
-    scrollToTopBtn.classList.add("show");
-  } else {
-    scrollToTopBtn.classList.remove("show");
-  }
+// Rest of your code...
+
+// Attach the scroll event listener conditionally for desktop screens
+if (isDesktop()) {
+  window.addEventListener("scroll", handleScrollToTopButton);
 }
-
-// Attach a scroll event listener to show/hide the button
-window.addEventListener("scroll", toggleScrollToTopButton);
-
-// Call toggleScrollToTopButton initially to check scroll position
-toggleScrollToTopButton();
-
-// function toggleMenu() {
-//   // Your existing toggleMenu function code here
-//   const menu = document.querySelector(".menu-links");
-//   const icon = document.querySelector(".hamburger-icon");
-//   menu.classList.toggle("open");
-//   icon.classList.toggle("open");
-//   // Adjust the navigation height when scrolling
-//   const nav = document.getElementById("desktop-nav");
-//   if (window.scrollY > 0) {
-//     nav.style.height = "10vh"; // Adjust this value as needed
-//   } else {
-//     nav.style.height = "17vh"; // Original height
-//   }
-// }
-
-// window.addEventListener("scroll", toggleMenu);
-
-// // Scroll to top button function
-// function scrollToTop() {
-//   window.scrollTo({
-//     top: 0,
-//     behavior: "smooth",
-//   });
-// }
-
-// // Handle scroll to top button visibility
-// window.addEventListener("scroll", function () {
-//   const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-//   if (window.scrollY > 100) {
-//     scrollToTopBtn.style.display = "block";
-//   } else {
-//     scrollToTopBtn.style.display = "none";
-//   }
-// });
-
-// // Check if the screen width is greater than a certain threshold (e.g., 768px)
-// function isDesktop() {
-//   return window.innerWidth > 768; // You can adjust the threshold as needed
-// }
-
-// // Attach the scroll event listener conditionally for desktop screens
-// if (isDesktop()) {
-//   window.addEventListener("scroll", toggleMenu);
-//   window.addEventListener("scroll", handleScrollToTopButton);
-// }
-
-// // Function to handle the mobile menu toggle
-// function toggleMenu() {
-//   const menu = document.querySelector(".menu-links");
-//   const icon = document.querySelector(".hamburger-icon");
-//   menu.classList.toggle("open");
-//   icon.classList.toggle("open");
-//   // Adjust the navigation height when scrolling
-//   const nav = document.getElementById("desktop-nav");
-//   if (window.scrollY > 0) {
-//     nav.style.height = "10vh"; // Adjust this value as needed
-//   } else {
-//     nav.style.height = "17vh"; // Original height
-//   }
-// }
-
-// // Function to handle scroll to top button visibility
-// function handleScrollToTopButton() {
-//   const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-//   if (window.scrollY > 100) {
-//     scrollToTopBtn.style.display = "block";
-//   } else {
-//     scrollToTopBtn.style.display = "none";
-//   }
-// }
-
-// // Function to scroll to the top of the page
-// function scrollToTop() {
-//   window.scrollTo({
-//     top: 0,
-//     behavior: "smooth",
-//   });
-// }
-
-// // Rest of your code...
-
-// // Attach the scroll event listener conditionally for desktop screens
-// if (isDesktop()) {
-//   window.addEventListener("scroll", handleScrollToTopButton);
-// }
