c8cd5ea762ab6211b4aa79b70ff87c2b8e0352c6
Richmelody <chigs1million@gmail.com>
2023-10-04 22:56:38 +0100

    worked on my css and js files

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ function toggleMenu() {
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+}
 
-  // Adjust the navigation height when scrolling
+  // Listen for the "scroll" event on the window
+window.addEventListener("scroll", function () {
+  // Get a reference to your navigation element by its ID
   const nav = document.getElementById("desktop-nav");
+
+  // Check the scroll position and adjust the height accordingly
   if (window.scrollY > 0) {
     nav.style.height = "10vh"; // Adjust this value as needed
   } else {
     nav.style.height = "17vh"; // Original height
   }
-}
+});
+
 
 // Scroll to top button function
 function scrollToTop() {
@@ -32,10 +38,10 @@ window.addEventListener("scroll", function () {
   }
 });
 
-// Check if the screen width is greater than a certain threshold (e.g., 768px)
-function isDesktop() {
-  return window.innerWidth > 768; // You can adjust the threshold as needed
-}
+// // Check if the screen width is greater than a certain threshold (e.g., 768px)
+// function isDesktop() {
+//   return window.innerWidth > 768; // You can adjust the threshold as needed
+// }
 
 // Attach the scroll event listener conditionally for desktop screens
 if (isDesktop()) {
@@ -43,42 +49,42 @@ if (isDesktop()) {
   window.addEventListener("scroll", handleScrollToTopButton);
 }
 
-// Function to handle the mobile menu toggle
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-  // Adjust the navigation height when scrolling
-  const nav = document.getElementById("desktop-nav");
-  if (window.scrollY > 0) {
-    nav.style.height = "10vh"; // Adjust this value as needed
-  } else {
-    nav.style.height = "17vh"; // Original height
-  }
-}
+// // Function to handle the mobile menu toggle
+// function toggleMenu() {
+//   const menu = document.querySelector(".menu-links");
+//   const icon = document.querySelector(".hamburger-icon");
+//   menu.classList.toggle("open");
+//   icon.classList.toggle("open");
+//   // Adjust the navigation height when scrolling
+//   const nav = document.getElementById("desktop-nav");
+//   if (window.scrollY > 0) {
+//     nav.style.height = "10vh"; // Adjust this value as needed
+//   } else {
+//     nav.style.height = "17vh"; // Original height
+//   }
+// }
 
-// Function to handle scroll to top button visibility
-function handleScrollToTopButton() {
-  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-  if (window.scrollY > 100) {
-    scrollToTopBtn.style.display = "block";
-  } else {
-    scrollToTopBtn.style.display = "none";
-  }
-}
+// // Function to handle scroll to top button visibility
+// function handleScrollToTopButton() {
+//   const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+//   if (window.scrollY > 100) {
+//     scrollToTopBtn.style.display = "block";
+//   } else {
+//     scrollToTopBtn.style.display = "none";
+//   }
+// }
 
-// Function to scroll to the top of the page
-function scrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-}
+// // Function to scroll to the top of the page
+// function scrollToTop() {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: "smooth",
+//   });
+// }
 
-// Rest of your code...
+// // // Rest of your code...
 
-// Attach the scroll event listener conditionally for desktop screens
-if (isDesktop()) {
-  window.addEventListener("scroll", handleScrollToTopButton);
-}
+// // // Attach the scroll event listener conditionally for desktop screens
+// // if (isDesktop()) {
+// //   window.addEventListener("scroll", handleScrollToTopButton);
+// // }
